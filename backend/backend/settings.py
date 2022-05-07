579fe682d4e233f0b989d870334d480201db7085
XiongLi <xiongli.eric@gmail.com>
2022-05-07 20:59:03 +0200

    implement fundtionality of protected routes and register user

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -82,7 +82,7 @@ SIMPLE_JWT = {
     'JTI_CLAIM': 'jti',
 
     'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
-    'SLIDING_TOKEN_LIFETIME': timedelta(days=30),
+    'SLIDING_TOKEN_LIFETIME': timedelta(days=1),
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
 
