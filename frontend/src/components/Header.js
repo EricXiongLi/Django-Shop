8dc15bf8ab3026418aab8ca480333287d34ebbc0
XiongLi <xiongli.eric@gmail.com>
2022-05-08 20:09:33 +0200

    add logout functionality

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,18 @@
 import React from "react";
 import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
 import {Link} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {logout} from "../actions/userActions";
 
 function Header() {
+  const userLogin = useSelector(state => state.userLogin)
+  const {userInfo} = userLogin
+  const dispatch = useDispatch()
+
+  const logoutHandler = () => {
+    dispatch(logout())
+  }
+
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
@@ -19,6 +29,21 @@ function Header() {
                   <i className="fas fa-shopping-cart">Cart</i>
                 </Nav.Link>
               </Link>
+
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id="username">
+                  <Link to="/profile">
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </Link>
+                  <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+
+                </NavDropdown>
+              ) : (
+                <Link to="/profile">
+                  <Nav.Link><i className="fas fa-user"></i> Login </Nav.Link>
+                </Link>
+              )}
+
               <Link to="/login">
                 <Nav.Link href="/login">
                   <i className="fas fa-user">Login</i>
