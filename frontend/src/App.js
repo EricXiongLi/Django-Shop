cd3fcb38642830564f0a85a6eb2de4abd6b930f4
XiongLi <xiongli.eric@gmail.com>
2022-05-08 19:56:20 +0200

    add user login page and functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,32 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import { Container } from "react-bootstrap";
+import {Container} from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Route } from "react-router-dom";
+import {BrowserRouter as Router} from "react-router-dom";
+import {Route} from "react-router-dom";
 import ProductScreen from "./screens/ProductScreen";
-import { Routes } from "react-router-dom";
+import {Routes} from "react-router-dom";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 function App() {
   return (
     <Router>
-      <Header />
+      <Header/>
       <main className="py-3">
         <Container>
           <Routes>
-            <Route path="/" element={<HomeScreen />} />
-            <Route path="/product/:id" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen/>}/>
+            <Route path="/login" element={<LoginScreen/>}/>
+            <Route path="/product/:id" element={<ProductScreen/>}/>
             <Route path="/cart">
-              <Route index element={<CartScreen />} />
-              <Route path=":id" element={<CartScreen />} />
+              <Route index element={<CartScreen/>}/>
+              <Route path=":id" element={<CartScreen/>}/>
             </Route>
           </Routes>
         </Container>
       </main>
-      <Footer />
+      <Footer/>
     </Router>
   );
 }
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -5,23 +5,20 @@ export const login = (email, password) => async (dispatch) => {
     dispatch({
       type:USER_LOGIN_REQUEST
     })
-
-    const {data} = await axios.post("http://127.0.0.1:8080/api/users/login/",{'username':email, 'passowrd':password},{
+    const {data} = await axios.post("http://127.0.0.1:8000/api/users/login/",{'username':email, 'password':password},{
       headers:{
         'Content-type':'application/json'
       }
     })
-
     dispatch({
       type:USER_LOGIN_SUCCESS,
       payload:data
     })
-
     localStorage.setItem('userInfo', JSON.stringify(data))
   } catch(error) {
     dispatch({
       type: USER_LOGIN_FAIL,
-      payload: error.response && error.response.data.detail ? error.response.data.detail : error.message,
+      payload: error.response && error.response.data ? error.response.data.detail : error.message,
     })
   }
 }
