8f1de2f9f77ed334c1770228de6a151e512bb739
XiongLi <xiongli.eric@gmail.com>
2022-05-14 21:35:31 +0200

    add orders in profile

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -1,7 +1,17 @@
 import axios from "axios";
 import {
-	ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_DETAILS_FAIL,
-	ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_PAY_FAIL, ORDER_PAY_REQUEST, ORDER_PAY_SUCCESS
+	ORDER_CREATE_FAIL,
+	ORDER_CREATE_REQUEST,
+	ORDER_CREATE_SUCCESS,
+	ORDER_DETAILS_FAIL,
+	ORDER_DETAILS_REQUEST,
+	ORDER_DETAILS_SUCCESS,
+	ORDER_LIST_MY_FAIL,
+	ORDER_LIST_MY_REQUEST,
+	ORDER_LIST_MY_SUCCESS,
+	ORDER_PAY_FAIL,
+	ORDER_PAY_REQUEST,
+	ORDER_PAY_SUCCESS
 } from "../constants/orderConstants";
 import {CART_CLEAE_ITEMS} from "../constants/cartConstants";
 
@@ -83,7 +93,7 @@ export const payOrder = (id, paymentResult) => async (dispatch, getState) => {
 			userLogin: {userInfo}
 		} = getState()
 
-		const {data} = await axios.put(`http://127.0.0.1:8000/api/orders/${id}/pay/`,paymentResult, {
+		const {data} = await axios.put(`http://127.0.0.1:8000/api/orders/${id}/pay/`, paymentResult, {
 			headers: {
 				'Content-type': 'application/json',
 				Authorization: `Bearer ${userInfo.token}`
@@ -98,6 +108,36 @@ export const payOrder = (id, paymentResult) => async (dispatch, getState) => {
 	} catch (error) {
 		dispatch({
 			type: ORDER_PAY_FAIL,
+			payload: error.response && error.response.data ? error.response.data.detail : error.message,
+		})
+	}
+}
+
+export const listMyOrders = () => async (dispatch, getState) => {
+	try {
+		dispatch({
+			type: ORDER_LIST_MY_REQUEST
+		})
+
+		const {
+			userLogin: {userInfo}
+		} = getState()
+
+		const {data} = await axios.get(`http://127.0.0.1:8000/api/orders/myorders/`, {
+			headers: {
+				'Content-type': 'application/json',
+				Authorization: `Bearer ${userInfo.token}`
+			}
+		})
+
+		dispatch({
+			type: ORDER_LIST_MY_SUCCESS,
+			payload: data
+		})
+
+	} catch (error) {
+		dispatch({
+			type: ORDER_LIST_MY_FAIL,
 			payload: error.response && error.response.data ? error.response.data.detail : error.message,
 		})
 	}
--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,102 +1,139 @@
 import React, {useEffect, useState} from 'react';
-import {Button, Col, Form, Row} from "react-bootstrap";
+import {Button, Col, Form, Row, Table} from "react-bootstrap";
 import {useDispatch, useSelector} from "react-redux";
-import {useNavigate} from "react-router-dom";
-import {getUserDetails, register, updateUserProfile} from "../actions/userActions";
+import {Link, useNavigate} from "react-router-dom";
+import {getUserDetails, updateUserProfile} from "../actions/userActions";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
 import {USER_UPDATE_PROFILE_RESET} from "../constants/userConstants";
+import {listMyOrders} from "../actions/orderActions";
+import {FaTimes} from "react-icons/fa";
 
 const ProfileScreen = () => {
 
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [confirmPassword, setConfirmPassword] = useState("")
-  const [message, setMessage] = useState("")
-  const dispatch = useDispatch()
-  const navigate = useNavigate();
+	const [name, setName] = useState("")
+	const [email, setEmail] = useState("")
+	const [password, setPassword] = useState("")
+	const [confirmPassword, setConfirmPassword] = useState("")
+	const [message, setMessage] = useState("")
+	const dispatch = useDispatch()
+	const navigate = useNavigate();
 
-  const userDetails = useSelector(state => state.userDetails)
-  const {error, loading, user} = userDetails
+	const userDetails = useSelector(state => state.userDetails)
+	const {error, loading, user} = userDetails
 
-  const userLogin = useSelector(state => state.userLogin)
-  const {userInfo} = userLogin
+	const userLogin = useSelector(state => state.userLogin)
+	const {userInfo} = userLogin
 
-  const userUpdateProfile = useSelector(state => state.userUpdateProfile)
-  const {success} = userUpdateProfile
+	const userUpdateProfile = useSelector(state => state.userUpdateProfile)
+	const {success} = userUpdateProfile
 
-  useEffect(() => {
-    if (!userInfo) {
-      navigate("/login")
-    } else {
-      if (!user || !user.name || success) {
-        dispatch({type:USER_UPDATE_PROFILE_RESET})
-        dispatch(getUserDetails("profile"))
-      } else {
-        setName(user.name)
-        setEmail(user.email)
-      }
-    }
-  }, [dispatch, window.history, user, success])
+	const orderListMy = useSelector(state => state.orderListMy)
+	const {loading: loadingOrders, error: errorOrders, orders} = orderListMy
 
-
-  const submitHandler = (e) => {
-    e.preventDefault()
-    if (password !== confirmPassword) {
-      setMessage("Password not match")
-    } else {
-      dispatch(updateUserProfile({'id': user._id, name, email, password}))
-      setMessage("")
-    }
-  }
+	useEffect(() => {
+		if (!userInfo) {
+			navigate("/login")
+		} else {
+			if (!user || !user.name || success) {
+				dispatch({type: USER_UPDATE_PROFILE_RESET})
+				dispatch(getUserDetails("profile"))
+				dispatch(listMyOrders())
+			} else {
+				setName(user.name)
+				setEmail(user.email)
+			}
+		}
+	}, [dispatch, window.history, user, success])
 
 
-  return (
-    <Row>
-      <Col md={3}>
-        <h2>User Profile</h2>
-        {message && <Message variant="danger">{message}</Message>}
-        {error && <Message variant="danger">{error}</Message>}
-        {loading && <Loader/>}
-        <Form onSubmit={submitHandler}>
-          <Form.Group controlId="name">
-            <Form.Label>Name</Form.Label>
-            <Form.Control required type="name" placeholder="Enter Name" value={name}
-                          onChange={(e) => setName(e.target.value)}>
-            </Form.Control>
-          </Form.Group>
+	const submitHandler = (e) => {
+		e.preventDefault()
+		if (password !== confirmPassword) {
+			setMessage("Password not match")
+		} else {
+			dispatch(updateUserProfile({'id': user._id, name, email, password}))
+			setMessage("")
+		}
+	}
 
-          <Form.Group controlId="email">
-            <Form.Label>Email Address</Form.Label>
-            <Form.Control required type="email" placeholder="Enter Email" value={email}
-                          onChange={(e) => setEmail(e.target.value)}>
-            </Form.Control>
-          </Form.Group>
 
-          <Form.Group controlId="password">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Enter Password" value={password}
-                          onChange={(e) => setPassword(e.target.value)}>
-            </Form.Control>
-          </Form.Group>
+	return (
+		<Row>
+			<Col md={3}>
+				<h2>User Profile</h2>
+				{message && <Message variant="danger">{message}</Message>}
+				{error && <Message variant="danger">{error}</Message>}
+				{loading && <Loader/>}
+				<Form onSubmit={submitHandler}>
+					<Form.Group controlId="name">
+						<Form.Label>Name</Form.Label>
+						<Form.Control required type="name" placeholder="Enter Name" value={name}
+													onChange={(e) => setName(e.target.value)}>
+						</Form.Control>
+					</Form.Group>
 
-          <Form.Group controlId="confirmPassword">
-            <Form.Label>Confirm Password</Form.Label>
-            <Form.Control type="password" placeholder="Confirm Your Password" value={confirmPassword}
-                          onChange={(e) => setConfirmPassword(e.target.value)}>
-            </Form.Control>
-          </Form.Group>
-          <Button type="submit" variant="primary">Update profile</Button>
-        </Form>
-      </Col>
+					<Form.Group controlId="email">
+						<Form.Label>Email Address</Form.Label>
+						<Form.Control required type="email" placeholder="Enter Email" value={email}
+													onChange={(e) => setEmail(e.target.value)}>
+						</Form.Control>
+					</Form.Group>
 
-      <Col md={9}>
-        <h2>My Orders</h2>
-      </Col>
+					<Form.Group controlId="password">
+						<Form.Label>Password</Form.Label>
+						<Form.Control type="password" placeholder="Enter Password" value={password}
+													onChange={(e) => setPassword(e.target.value)}>
+						</Form.Control>
+					</Form.Group>
 
-    </Row>
-  );
+					<Form.Group controlId="confirmPassword">
+						<Form.Label>Confirm Password</Form.Label>
+						<Form.Control type="password" placeholder="Confirm Your Password" value={confirmPassword}
+													onChange={(e) => setConfirmPassword(e.target.value)}>
+						</Form.Control>
+					</Form.Group>
+					<Button type="submit" variant="primary">Update profile</Button>
+				</Form>
+			</Col>
+
+			<Col md={9}>
+				<h2>My Orders</h2>
+				{loadingOrders ? <Loader/> : errorOrders ? <Message variant="danger">{errorOrders}</Message> :
+					<Table striped responsive className="table-sm">
+						<thead>
+						<tr>
+							<th>ID</th>
+							<th>Date</th>
+							<th>Total</th>
+							<th>Paid</th>
+							<th>Delievered</th>
+							<th></th>
+						</tr>
+						</thead>
+
+						<tbody>
+						{orders.map(order => (
+							<tr key={order._id}>
+								<td>{order._id}</td>
+								<td>{order.createdAt.substring(0, 10)}</td>
+								<td>{order.totalPrice}</td>
+								<td>{order.isPaid ? order.paidAt : <FaTimes color="red"/>}</td>
+								<td>
+									<Link to={`/order/${order._id}`}>
+										<Button className="btn-sm">DETAILS</Button>
+									</Link>
+								</td>
+
+							</tr>
+						))}
+						</tbody>
+					</Table>
+				}
+			</Col>
+
+		</Row>
+	);
 };
 
 export default ProfileScreen;
