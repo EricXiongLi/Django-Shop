5c0939ac792d8341f2e63375a41b1ee871a11deb
XiongLi <xiong.li@uzh.ch>
2022-07-14 11:30:20 +0200

    add create review endpoint

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -80,3 +80,39 @@ def uploadImage(request):
     product.image = request.FILES.get('image')
     product.save()
     return Response('Image was uploaded')
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def createProductReview(request, pk):
+    user = request.user
+    product = Product.objects().get(_id=pk)
+    data = request.data
+    # 1. review already exist
+    alreadyExists = product.review_set.filter(user=user).exists()
+    if alreadyExists:
+        content = {'details': 'Product already reviewed'}
+        return Response(content, status=status.HTTP_400_BAD_REQUEST)
+    # 2. No rating or 0
+    elif data['rating'] == 0:
+        content = {'details': 'Please select a rating'}
+        return Response(content, staus=status.HTTP_400_BAD_REQUEST)
+    # 3. create Review
+    else:
+        review = Review.objects.create(
+            user=user,
+            product=product,
+            name=user.first_name,
+            rating=data['rating'],
+            comment=data['comment'],
+        )
+        reviews = product.review_set.all()
+        product.numReviews = len(reviews)
+
+        total = 0
+        for i in reviews:
+            total += i.rating
+        product.rating = total / len(reviews)
+        product.save()
+
+        return Response({'detail': 'Review Added'})
